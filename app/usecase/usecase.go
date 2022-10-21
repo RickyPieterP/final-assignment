8698c2566451a70c232e285ff20534704488cdbc
fadli.ramadhan <fadli.ramadhan@mncbank.co.id>
2022-10-21 21:11:03 +0700

    comment init

--- app/usecase/usecase.go
+++ app/usecase/usecase.go
@@ -8,21 +8,12 @@ import (
 type Usecase interface {
 	RegisterUser(in request.RegisterUserReq) (out response.RegisterUserRes, httpStatus int)
 	LoginUser(in request.LoginUserReq) (out *response.LoginUserResp, httpStatus int, err error)
-	UpdateUser(in request.UpdateUserReq) (out *response.UpdateUserRes, httpStatus int, err error)
-	DeleteUser(in Token) (out *response.DeleteUser, httpStatus int, err error)
 	CreatePhoto(in *request.CreatePhotoReq) (out *response.CreatePhotoResp, httpStatus int, err error)
 	FindPhoto(in *request.FindReq) (out []response.FindPhotoResp, httpStatus int, err error)
 	UpdatePhoto(in *request.UpdatePhoto) (out *response.UpdatePhotoResp, err error)
 	DeletePhoto(in, user_id int) (out *response.DeletePhoto, err error)
-<<<<<<< HEAD
 	CreateComment(in *request.CreateCommentReq) (out *response.CreateCommentResp, httpStatus int, err error)
 	FindComment(in *request.FindCommentReq) (out []response.FindCommentResp, httpStatus int, err error)
 	UpdateComment(in *request.UpdateComment) (out *response.UpdateCommentResp, err error)
 	DeleteComment(in, user_id int) (out *response.DeleteComment, err error)
-=======
-	CreateSocialMedia(in request.CreateSocialMediaReq, userID any) (out response.CreateSocialMediaRes, httpStatus int, err error)
-	FindSocialMedia(in any) (out response.FindSocialMediaRes, httpStatus int, err error)
-	UpdateSocialMedia(in request.UpdateSocialMediaReq, userID any, socialMediaID any) (out response.UpdateSocialMediaRes, httpStatus int, err error)
-	DeleteSocialMedia(socialMediaID any) (out string, httpStatus int, err error)
->>>>>>> e1086dbf4a7be4024ac466c15b0c2f840136ae9f
 }
